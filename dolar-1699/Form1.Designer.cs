69bfa93a1432cc64f1d61d9008c4ef39e6f8d587
busrailhan <150009386+busrailhan@users.noreply.github.com>
2023-11-06 11:47:44 +0300

    Add files via upload

---
+++ dolar-1699/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace dolar_1699
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblßonuc = new System.Windows.Forms.Label();
+            this.txtAdet = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.txtBirim = new System.Windows.Forms.TextBox();
+            this.btnCevir = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(39, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "adet";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(39, 75);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "birim";
+            // 
+            // lblßonuc
+            // 
+            this.lblßonuc.AutoSize = true;
+            this.lblßonuc.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblßonuc.Location = new System.Drawing.Point(49, 162);
+            this.lblßonuc.Name = "lblßonuc";
+            this.lblßonuc.Size = new System.Drawing.Size(17, 20);
+            this.lblßonuc.TabIndex = 2;
+            this.lblßonuc.Text = "tl";
+            this.lblßonuc.Click += new System.EventHandler(this.lblßonuc_Click);
+            // 
+            // txtAdet
+            // 
+            this.txtAdet.Location = new System.Drawing.Point(119, 40);
+            this.txtAdet.Name = "txtAdet";
+            this.txtAdet.Size = new System.Drawing.Size(100, 20);
+            this.txtAdet.TabIndex = 3;
+            this.txtAdet.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(0, 0);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 4;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(0, 0);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 5;
+            // 
+            // txtBirim
+            // 
+            this.txtBirim.Location = new System.Drawing.Point(119, 75);
+            this.txtBirim.Name = "txtBirim";
+            this.txtBirim.Size = new System.Drawing.Size(100, 20);
+            this.txtBirim.TabIndex = 6;
+            // 
+            // btnCevir
+            // 
+            this.btnCevir.Location = new System.Drawing.Point(235, 145);
+            this.btnCevir.Name = "btnCevir";
+            this.btnCevir.Size = new System.Drawing.Size(75, 23);
+            this.btnCevir.TabIndex = 7;
+            this.btnCevir.UseVisualStyleBackColor = true;
+            this.btnCevir.Click += new System.EventHandler(this.btnCevir_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnCevir);
+            this.Controls.Add(this.txtBirim);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.txtAdet);
+            this.Controls.Add(this.lblßonuc);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblßonuc;
+        private System.Windows.Forms.TextBox txtAdet;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox txtBirim;
+        private System.Windows.Forms.Button btnCevir;
+    }
+}
+
